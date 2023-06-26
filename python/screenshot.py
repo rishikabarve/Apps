3d43a0aafbb6051c33639363a2f183fc5832a8d6
rishikabarve <rishikabarve8@gmail.com>
2023-06-26 13:28:38 +0530

    initial commit

---
+++ python/screenshot.py
@@ -0,0 +1,27 @@
+import time
+import pyautogui
+import tkinter as tk
+def screenshot():
+    name=int(round(time.time()*1000))
+    name='C:/Users/barve/Documents/python/screenshot data{}.png'.format(name)
+    img = pyautogui.screenshot(name)
+    img.show()
+    
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+button = tk.Button(
+    frame, text="Take Screenshot",
+    command = screenshot
+)
+button.pack(side=tk.LEFT)
+close= tk.Button(
+    frame, text="Quit",
+    command = quit
+)
+close.pack(side=tk.LEFT)
+
+root.mainloop()
+
+    
